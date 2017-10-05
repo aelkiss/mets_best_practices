eee0e86a758effe9c9acc6622a7db3f882587860
Aaron Elkiss <aelkiss@umich.edu>
2017-10-05 16:44:30 -0400

    make examples concrete for dmdsec_id_spec.rb

--- spec/dmdsec_id_spec.rb
+++ spec/dmdsec_id_spec.rb
@@ -1,18 +1,59 @@
 require 'spec_helper'
 
 describe 'dmdsec-id-checks' do
-  dmdsec_adder = lambda do |doc,_,id| 
-    add_dmdsec(doc,id)
+  let(:xml_doc) { minimal_mets_template(innards) }
+
+  context "with one unreferenced dmdSec" do
+    let(:innards) do <<EOT
+<dmdSec ID="DMD1" />
+<structMap>
+<div>
+</structMap>
+EOT
+    end
+
+    it_behaves_like "one schematron error", /WARNING: The dmdSec with ID "DMD1" is never referenced by a DMDID attribute/
   end
 
-  it_behaves_like "all ids are referenced", 'DMDID','dmdSec','div',dmdsec_adder
+  context "with one properly referenced dmdSec" do
+    let(:innards) do <<EOT
+    <dmdSec ID="DMD1" />
 
-  dmdsec_doc_builder = lambda do |doc,id|
-    add_mdsec(doc,'techMD',id)
-    doc.xpath('//xmlns:div').first['DMDID'] = id
-    doc.xpath('//xmlns:div').first['ADMID'] = id
+    <structMap>
+      <div DMDID='DMD1' />
+    </structMap>
+EOT
+    end
+
+    it_behaves_like "no errors"
   end
 
-  it_behaves_like "idref type checking", "DMDID", ['dmdSec'], 'techMD', dmdsec_doc_builder
+  context "with two dmdSecs, one referenced, one not" do
+    let(:innards) do <<EOT
+    <dmdSec ID="DMD1" />
+    <dmdSec ID="DMD2" />
+
+    <structMap>
+      <div DMDID="DMD1"  />
+    </structMap>
+EOT
+    end
+
+    it_behaves_like "one schematron error", /WARNING: The dmdSec with ID "DMD2" is never referenced by a DMDID attribute/
+  end
+
+  context "with a dmdid referencing a non-dmdsec" do
+    let(:innards) do <<EOT
+   <amdSec>
+     <techMD ID='TMD1' />
+   </amdSec>
+   <structMap>
+     <div DMDID='TMD1' ADMID='TMD1' />
+   </structMap>
+EOT
+    end
+
+    it_behaves_like "one schematron error", /ERROR: The DMDID "TMD1" should reference a dmdSec, not a techMD/
+  end
 
 end
