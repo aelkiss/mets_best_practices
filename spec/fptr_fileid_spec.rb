2edb681b78b27a6885f874c7e32ccded5f367c9c
Aaron Elkiss <aelkiss@umich.edu>
2017-10-05 17:20:29 -0400

    concretify fptr_fileid specs

--- spec/fptr_fileid_spec.rb
+++ spec/fptr_fileid_spec.rb
@@ -2,53 +2,86 @@ require 'spec_helper'
 
 describe 'fptr-fileid-checks' do
 
-	context 'with a fptr element' do
+  context 'with a fptr element' do
+    let(:xml_doc) do
+      minimal_mets_template(innards)
+    end
 
-		context 'with FILEID and no children' do
-			let(:xml_doc) do
-				minimal_mets.tap do |doc|
-					add_filegrp(doc).add_child(create_node(doc,'file',{ID: 'file1'}))
-					first_div(doc).add_child(create_node(doc,'fptr',{FILEID: 'file1'}))
-				end
-			end
+    context 'with FILEID and no children' do
+      let(:innards) do <<EOT
+        <fileSec>
+          <fileGrp>
+            <file ID="file1" />
+          </fileGrp>
+        </fileSec>
+        <structMap>
+          <div>
+            <fptr FILEID="file1" />
+          </div>
+        </structMap>
+      
+EOT
+      end
 
-			it_behaves_like 'no errors'
-		end
+      it_behaves_like 'no errors'
+    end
 
-		context 'with FILEID and children' do
-			let(:xml_doc) do
-				minimal_mets.tap do |doc|
-					add_filegrp(doc).add_child(create_node(doc,'file',{ID: 'file1'}))
-					first_div(doc).add_child(create_node(doc,'fptr',{FILEID: 'file1'}))
-						.add_child(create_node(doc,'area',{FILEID: 'file1'}))
-				end
-			end
+    context 'with FILEID and children' do
+      let(:innards) do <<EOT
+        <fileSec>
+          <fileGrp>
+            <file ID="file1" />
+          </fileGrp>
+        </fileSec>
+        <structMap>
+          <div>
+            <fptr FILEID="file1">
+              <area FILEID="file1" />
+            </fptr>
+          </div>
+        </structMap>
+      
+EOT
+      end
 
-			it_behaves_like 'one schematron error', /ERROR: A fptr element should only have a FILEID attribute value if it does not have a child area, par or seq element./
-		end
+      it_behaves_like 'one schematron error', /ERROR: A fptr element should only have a FILEID attribute value if it does not have a child area, par or seq element./
+    end
 
-		context 'with children but no FILEID' do
-			let(:xml_doc) do
-				minimal_mets.tap do |doc|
-					add_filegrp(doc).add_child(create_node(doc,'file',{ID: 'file1'}))
-					first_div(doc).add_child(create_node(doc,'fptr'))
-						.add_child(create_node(doc,'area',{FILEID: 'file1'}))
-				end
-			end
+    context 'with children but no FILEID' do
+      let(:innards) do <<EOT
+        <fileSec>
+          <fileGrp>
+            <file ID="file1" />
+          </fileGrp>
+        </fileSec>
+        <structMap>
+          <div>
+            <fptr>
+              <area FILEID="file1" />
+            </fptr>
+          </div>
+        </structMap>
+      
+EOT
+      end
 
-			it_behaves_like 'no errors'
-		end
+      it_behaves_like 'no errors'
+    end
 
-		context 'with no child and no FILEID' do
-			let(:xml_doc) do
-				minimal_mets.tap do |doc|
-					first_div(doc).add_child(create_node(doc,'fptr'))
-				end
-			end
+    context 'with no child and no FILEID' do
+      let(:innards) do <<EOT
+        <structMap>
+          <div>
+            <fptr />
+          </div>
+        </structMap>
+      
+EOT
+      end
 
-			it_behaves_like 'one schematron error', /ERROR: A fptr element should have a FILEID attribute if it does not have child area, par, or seq elements./
-		end
+      it_behaves_like 'one schematron error', /ERROR: A fptr element should have a FILEID attribute if it does not have child area, par, or seq elements./
+    end
 
-	end
+  end
 
 end
